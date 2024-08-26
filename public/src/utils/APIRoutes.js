e56d44e9df174112c7700d4b714d47bd8552c1a1
21620023k <dangkhoa3348@gmail.com>
2024-08-27 03:21:01 +0700

    config cors socket server

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,5 +1,6 @@
 //export const host = "http://localhost:5000";
-export const host ="https://chat-social-theta.vercel.app"
+//export const host ="https://chat-social-theta.vercel.app"
+export const host =process.env.URL_BACKEND;
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
