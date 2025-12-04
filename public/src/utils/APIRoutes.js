01fe6bdaaa3ac8d4318359a2fff70ac66d757e01
ducnmm <mauduckiengiang@gmail.com>
2025-12-05 00:29:57 +0700

    update code

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,6 +1,6 @@
 export const host = process.env.REACT_APP_API_URL;
 
-// Auth routes
+// Auth routes 1
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
